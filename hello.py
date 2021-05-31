e1850f243a7e30ca6004b07b3bd49c543eb4a5b5
Scot Newbury <scot@scotnewbury.com>
2021-05-31 13:46:21 -0400

    added shorter version of string reverse

--- hello.py
+++ hello.py
@@ -1,16 +1,20 @@
 i = 0
 new_name = ''
+another_name = ''
+
 name = input('What is your name? ')
-print('Hello ' + name + '. Welcome to the Matrix!')
+print('Hello ' + name + '. Welcome to the Matrix!\n\n')
 
 # test formatted string
-print(f'Hey {name}, welcome to the jungle!')
+print(f'Hey {name}, welcome to the jungle!\n\n')
 
 # looping through string
-# also reversed the orger
+# also reversed the order
 while len(name) > i:
-    print(name[i])
     new_name = name[i] + new_name
     i += 1
 
-print(f'This is the new name: {new_name}')
+another_name = name[::-1]  # quicker way to reverse a string
+
+print(f'This is the new name: {new_name}\n\n')
+print(f'This is another new name: {another_name}\n\n')

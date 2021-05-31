33729005b252f4dee5f08564b9de0b957da68ac5
Scot Newbury <scot@scotnewbury.com>
2021-05-31 12:03:53 -0400

    added function to reverse string

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
+i = 0
+new_name = ''
 name = input('What is your name? ')
 print('Hello ' + name + '. Welcome to the Matrix!')
 
 # test formatted string
 print(f'Hey {name}, welcome to the jungle!')
 
+# looping through string
+while len(name) > i:
+    print(name[i])
+    new_name = name[i] + new_name
+    i += 1
+
+print(f'This is the new name: {new_name}')

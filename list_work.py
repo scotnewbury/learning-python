40bdf8cd5f9060ca9759bc6fbd36f0e40b9faf37
Scot Newbury <scot@scotnewbury.com>
2021-06-05 23:24:33 -0400

    added challenge exercise from course

---
+++ list_work.py
@@ -0,0 +1,27 @@
+# just testing out lists
+
+# loop = 0
+# simple_list = [
+#   'apple',
+#   'banana',
+#   'cherry',
+#   'date',
+#   'eggplant'
+# ]
+# simple_list_length = len(simple_list)
+
+# # print(len(simple_list))
+
+# # loop through and print
+
+# while loop < simple_list_length:
+#   print(f'{simple_list[loop]}')
+#   loop +=1
+
+# challenge exercise lecture 43
+# using this list: 
+basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]]
+# access "Oranges" and print it:
+
+print(basket[1][1][0])
+

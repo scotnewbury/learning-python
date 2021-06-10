6df814ed27afbd4121995b022cf8cc546eb2ae25
Scot Newbury <scot@scotnewbury.com>
2021-06-10 00:19:15 -0400

    lecture exercise 54 - step 2 complete

---
+++ lecture54.py
@@ -0,0 +1,21 @@
+#1 Create a user profile for your new game. This user profile will be stored in a dictionary with keys: 'age', 'username', 'weapons', 'is_active' and 'clan'
+
+user_profile = {
+  'age': 20,
+  'username': 'Johnny',
+  'weapons': ['sword', 'axe', 'bow'],
+  'is_active': True,
+  'clan': 'Bone Crushers'
+}
+
+#2 iterate and print all the keys in the above user.
+print(user_profile.keys())
+
+#3 Add a new weapon to your user
+
+
+#4 Add a new key to include 'is_banned'. Set it to false
+
+#5 Ban the user by setting the previous key to True
+
+#6 create a new user2 my copying the previous user and update the age value and username value. 
